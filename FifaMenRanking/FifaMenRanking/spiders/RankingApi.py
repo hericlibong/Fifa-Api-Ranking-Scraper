5dc295fc755a78010bc9c541644adb8f323d370b
hericlibong <hericlibong@gmail.com>
2024-07-19 05:45:11 +0200

    added scraping

--- FifaMenRanking/FifaMenRanking/spiders/RankingApi.py
+++ FifaMenRanking/FifaMenRanking/spiders/RankingApi.py
@@ -5,13 +5,24 @@ import json
 class RankingapiSpider(scrapy.Spider):
     name = "RankingApi"
     allowed_domains = ["fifa.com"]
-    start_urls = ["https://inside.fifa.com/api/ranking-overview?locale=en&dateId=id14443"]
+    start_urls = ["https://inside.fifa.com/fifa-world-ranking/men?dateId=id14443"]
 
     def parse(self, response):
+        script_content = response.xpath("//script[contains(text(), 'date')]/text()").extract_first()
+        date_list = json.loads(script_content)
+        date_list = date_list['props']['pageProps']['pageData']['ranking']['dates']
+        for year_data in date_list:
+            for date_item in year_data['dates']:
+                url = f"https://inside.fifa.com/api/ranking-overview?locale=en&dateId={date_item['id']}"
+                date = date_item ['iso']
+                yield scrapy.Request(url=url, callback=self.parse_ranking_data, meta={'date':date})
+
+    def parse_ranking_data(self, response):
         data = json.loads(response.body)
         base_url = "https://inside.fifa.com"
         for ranking_data in data['rankings']:
             yield {
+                "date": response.meta['date'],
                 "country": ranking_data['rankingItem']['name'],
                 "rank": ranking_data['rankingItem']['rank'],
                 "previous_rank": ranking_data['rankingItem']['previousRank'],
